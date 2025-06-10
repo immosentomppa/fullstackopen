24daff9fdabf9cf4d6789d46b218ce4a90551967
Tommi Immonen <taimmo@utu.fi>
2025-06-11 00:14:42 +0300

    changes folder structure to make render upload efficient

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -31,6 +31,7 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -82,6 +83,25 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note)
 
   response.json(note)
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const newNote = {
+    content: body.content,
+    important: body.important || false,
+    id: id,
+  }
+  notes = notes.map(note => (note.id === id ? newNote : note))
+
+  response.json(newNote)
 })
 
 const unknownEndpoint = (request, response) => {
