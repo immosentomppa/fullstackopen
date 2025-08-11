6bb49aecb35afbc8f958c14cc520d4b514ba1fe4
Tommi Immonen <taimmo@utu.fi>
2025-08-11 04:20:40 +0300

    feat: implements graphql testing server+client for part8

---
+++ part8/graphql-testing/client/src/App.jsx
@@ -0,0 +1,82 @@
+import { useQuery, useMutation, useSubscription } from "@apollo/client"
+import { ALL_PERSONS } from "./queries"
+import { useState } from "react"
+import { useApolloClient } from "@apollo/client"
+
+import Persons from "./components/Persons"
+import PersonForm from "./components/PersonForm"
+import PhoneForm from "./components/PhoneForm"
+import Notify from "./components/Notify"
+import LoginForm from "./components/LoginForm"
+
+// function that takes care of manipulating cache
+export const updateCache = (cache, query, addedPerson) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    }
+  })
+}
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+  const result = useQuery(ALL_PERSONS)
+
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data, client }) => {
+      const addedPerson = data.data.personAdded
+      notify(`${addedPerson.name} added`)
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+    },
+  })
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+}
+
+export default App
