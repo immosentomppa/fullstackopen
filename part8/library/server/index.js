b9e925b60e290902fa6ca48efa598f00acf46a5c
Tommi Immonen <taimmo@utu.fi>
2025-08-11 04:22:34 +0300

    feat: implements graphql tasks 8.1-8.22 of part8

---
+++ part8/library/server/index.js
@@ -0,0 +1,60 @@
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const { GraphQLError } = require("graphql")
+
+const User = require("./models/user")
+const jwt = require("jsonwebtoken")
+const typeDefs = require("./schema")
+const resolvers = require("./resolvers")
+
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
+require("dotenv").config()
+const MONGODB_URI = process.env.MONGODB_URI
+console.log("connecting to", MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB")
+  })
+  .catch((error) => {
+    console.log("error connection to MongoDB:", error.message)
+  })
+
+/*
+ * Suomi:
+ * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+ * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+ *
+ * English:
+ * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * However, for simplicity, we will store the author's name in connection with the book
+ *
+ * Spanish:
+ * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
+ * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
+ */
+
+/*
+  you can remove the placeholder query once your first one has been implemented 
+*/
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith("Bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
